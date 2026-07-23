74ed746f6c2159b04d3b68dff735d3adb76a3176
Harith <harithdn456@gmail.com>
2026-07-23 18:25:10 +0530

    update

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Title from './Title'
+import Description from './Description';
+
+
+
+
+function App() {
+
+  return(
+    <>
+
+     <Title/>
+     <Description/>
+    </>
+     
+    
+  );
+
+}
+
+
+export default App
---
+++ React/basic-react-app/src/Description.jsx
@@ -0,0 +1,7 @@
+function Description()
+{
+  let name="Harith"
+  return <p>I am {name}! I'm {19+1}</p>
+}
+
+export default Description
